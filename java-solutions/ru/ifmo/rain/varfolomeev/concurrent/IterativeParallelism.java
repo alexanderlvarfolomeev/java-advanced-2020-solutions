81eb47c26f3a6bdafc1e90c22bc7da582aeccddd
alexanderlvarfolomeev <vas.aleks.v@gmail.com>
2020-04-10 13:00:37 +0300

    fff

--- java-solutions/ru/ifmo/rain/varfolomeev/concurrent/IterativeParallelism.java
+++ java-solutions/ru/ifmo/rain/varfolomeev/concurrent/IterativeParallelism.java
@@ -28,17 +28,6 @@ public class IterativeParallelism implements AdvancedIP {
         this.parallelMapper = null;
     }
 
-    /**
-     * Returns maximum value.
-     *
-     * @param threadCount number or concurrent threads.
-     * @param values      values to get maximum of.
-     * @param comparator  value comparator.
-     * @param <T>         value type.
-     * @return maximum of given values
-     * @throws InterruptedException   if executing thread was interrupted.
-     * @throws NoSuchElementException if not values are given.
-     */
     @Override
     public <T> T maximum(int threadCount, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
         if (values.isEmpty()) {
@@ -48,32 +37,11 @@ public class IterativeParallelism implements AdvancedIP {
         return parallelFunction(threadCount, values, function, function);
     }
 
-    /**
-     * Returns minimum value.
-     *
-     * @param threadCount number or concurrent threads.
-     * @param values      values to get minimum of.
-     * @param comparator  value comparator.
-     * @param <T>         value type.
-     * @return minimum of given values
-     * @throws InterruptedException   if executing thread was interrupted.
-     * @throws NoSuchElementException if not values are given.
-     */
     @Override
     public <T> T minimum(int threadCount, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
         return maximum(threadCount, values, comparator.reversed());
     }
 
-    /**
-     * Returns whether all values satisfies predicate.
-     *
-     * @param threadCount number or concurrent threads.
-     * @param values      values to test.
-     * @param predicate   test predicate.
-     * @param <T>         value type.
-     * @return whether all values satisfies predicate or {@code true}, if no values are given.
-     * @throws InterruptedException if executing thread was interrupted.
-     */
     @Override
     public <T> boolean all(int threadCount, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
         return parallelFunction(
@@ -83,29 +51,11 @@ public class IterativeParallelism implements AdvancedIP {
         );
     }
 
-    /**
-     * Returns whether any of values satisfies predicate.
-     *
-     * @param threadCount number or concurrent threads.
-     * @param values      values to test.
-     * @param predicate   test predicate.
-     * @param <T>         value type.
-     * @return whether any value satisfies predicate or {@code false}, if no values are given.
-     * @throws InterruptedException if executing thread was interrupted.
-     */
     @Override
     public <T> boolean any(int threadCount, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
         return !all(threadCount, values, predicate.negate());
     }
 
-    /**
-     * Join values to string.
-     *
-     * @param threadCount number of concurrent threads.
-     * @param values      values to join.
-     * @return list of joined result of {@link #toString()} call on each value.
-     * @throws InterruptedException if executing thread was interrupted.
-     */
     @Override
     public String join(int threadCount, List<?> values) throws InterruptedException {
         return parallelFunction(
@@ -115,16 +65,6 @@ public class IterativeParallelism implements AdvancedIP {
         );
     }
 
-    /**
-     * Filters values by predicate.
-     *
-     * @param threadCount number of concurrent threads.
-     * @param values      values to filter.
-     * @param predicate   filter predicate.
-     * @param <T>         value type.
-     * @return list of values satisfying given predicated. Order of values is preserved.
-     * @throws InterruptedException if executing thread was interrupted.
-     */
     @Override
     public <T> List<T> filter(final int threadCount, final List<? extends T> values, final Predicate<? super T> predicate) throws InterruptedException {
         return parallelFunction(
@@ -134,17 +74,6 @@ public class IterativeParallelism implements AdvancedIP {
         );
     }
 
-    /**
-     * Maps values.
-     *
-     * @param threadCount number of concurrent threads.
-     * @param values      values to filter.
-     * @param f           mapper function.
-     * @param <T>         value type.
-     * @param <U>         result type.
-     * @return list of values mapped by given function.
-     * @throws InterruptedException if executing thread was interrupted.
-     */
     @Override
     public <T, U> List<U> map(final int threadCount, final List<? extends T> values, final Function<? super T, ? extends U> f) throws InterruptedException {
         return parallelFunction(
@@ -154,34 +83,13 @@ public class IterativeParallelism implements AdvancedIP {
         );
     }
 
-
-    /**
-     * Reduces values using monoid.
-     *
-     * @param threadCount number of concurrent threads.
-     * @param values      values to reduce.
-     * @param monoid      monoid to use.
-     * @param <T>         value type.
-     * @return values reduced by provided monoid or {@link Monoid#getIdentity() identity} if not values specified.
-     * @throws InterruptedException if executing thread was interrupted.
-     */
+    @Override
     public <T> T reduce(final int threadCount, List<T> values, final Monoid<T> monoid) throws InterruptedException {
         Function<Stream<T>, T> function = stream -> stream.reduce(monoid.getIdentity(), monoid.getOperator());
         return parallelFunction(threadCount, values, function, function);
     }
 
-    /**
-     * Maps and reduces values using monoid.
-     *
-     * @param threadCount number of concurrent threads.
-     * @param values      values to reduce.
-     * @param lift        mapping function.
-     * @param monoid      monoid to use.
-     * @param <T>         value type.
-     * @param <R>         result type.
-     * @return values reduced by provided monoid or {@link Monoid#getIdentity() identity} if not values specified.
-     * @throws InterruptedException if executing thread was interrupted.
-     */
+    @Override
     public <T, R> R mapReduce(final int threadCount, final List<T> values, final Function<T, R> lift, final Monoid<R> monoid) throws InterruptedException {
         return reduce(threadCount, map(threadCount, values, lift), monoid);
     }
