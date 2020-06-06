168e52454d6eb23c0ef32d592537a81565d6f946
Varfolomeev Alexander <vas.aleks.v@gmail.com>
2020-06-06 13:03:54 +0300

    ffF

--- scripts/bank/runTestsWithLib.cmd
+++ scripts/bank/runTestsWithLib.cmd
@@ -4,6 +4,6 @@ call %script_folder%build.cmd
 
 java  -cp _build;%lib_folder%\junit-4.11.jar;%lib_folder%\hamcrest-core-1.3.jar org.junit.runner.JUnitCore ru.ifmo.rain.varfolomeev.bank.BankTests
 
-start %script_folder%delete_built.cmd
+start %script_folder%delete_build.cmd
 
 exit %errorlevel%
--- scripts/bank/startServer.cmd
+++ scripts/bank/startServer.cmd
@@ -1,7 +1,5 @@
 set script_folder=%~dp0
 
-call %script_folder%build.cmd
-
 call %script_folder%runRMIregistry.cmd
 
 java -cp _build ru.ifmo.rain.varfolomeev.bank.server.Server
